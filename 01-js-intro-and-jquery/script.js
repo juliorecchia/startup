ba1a3736b33d31a44b265b644643c5d0c4f2a004
juliorecchia <juorecchia@gmail.com>
2015-02-26 02:41:36 -0300

    mejoras

--- 01-js-intro-and-jquery/script.js
+++ 01-js-intro-and-jquery/script.js
@@ -1,10 +1,18 @@
+
 $(document).ready(function(){
 
  $('.hidden').show(3000,function(){
  	$('.alias').focus();
- }); 
- 
 
+ 	var $obj=('a');
+		$('.alias').bind("myCustomEvent", function(){
+		$( this ).text( $obj );
+		});
+
+		$( '.boton' ).click(function() {
+		$( '.alias' ).trigger( "myCustomEvent");
+		});
+ 	}); 
  
 });
  
