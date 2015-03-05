c11d1edb8b54de7f095551314d3523cd2512c0b1
juliorecchia <juorecchia@gmail.com>
2015-03-05 18:57:30 -0300

    scripts added

---
+++ 03-amd-requirejs/scripts/movie.js
@@ -0,0 +1,30 @@
+define('module_movie',["module_director"],function(module_director)
+{
+			
+			movie =function(titulo,genero)
+			{
+				this.titulo=titulo;
+				this.genero=genero;
+
+				var titulo;
+				var genero;
+				var director;
+				
+				this.set=function(titulo,genero)
+				{ 
+					this.titulo=titulo;
+					this.genero=genero;
+				}
+				this.get=function()
+				{
+				console.log("Titulo pelicula: "+this.titulo+", Genero: "+this.genero);
+				}
+				this.addDirector=function(director)
+				{
+		 			//codigo para agregar director a la peli
+ 					
+				}	
+
+				return module_movie;
+			}
+});
