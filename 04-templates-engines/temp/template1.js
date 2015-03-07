89d5e7cc857bf72a12def4686a37fc18af8350fc
juliorecchia <juorecchia@gmail.com>
2015-03-07 16:39:53 -0300

    Handlebars funciona

--- 04-templates-engines/temp/template1.js
+++ 04-templates-engines/temp/template1.js
@@ -4,18 +4,16 @@ $(document).ready(function(){
 	var source=$("#entry-template").html();
 	var template=Handlebars.compile(source);
 
-
-	function readJson()
-	{
-		$.getJSON("data.json",function(data){
+		$.getJSON("data.json",function(data)
+		{
 
 			Handlebars.registerHelper('fullName',function (person) 
 			{
 				return person.firstName+" "+person.lastName;	
 			});	
-				$('body').append(template(html));
+				$('body').append(template(data));
 		});
-	}
+	
 	//var data = $.getJSON({ 
 	//		url: 'data.json'
 	//	});
